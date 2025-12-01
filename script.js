d2e0e8dc7c406ab954712ef40950a90e57649b36
eastongifford-ship-it <eastongifford@ucas-edu.net>
2025-12-01 14:42:19 -0700

    final

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleContent() {
+    const extra = document.getElementById("extra");
+    if (extra.style.display === "none" || extra.style.display === "") {
+      extra.style.display = "block";
+    } else {
+      extra.style.display = "none";
+    }
+  }
+  
