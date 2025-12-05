d7b1c18d2210b645dc4ebfade67e28459b67556a
eastongifford-ship-it <eastongifford@ucas-edu.net>
2025-12-05 14:42:45 -0700

    67

--- final/script.js
+++ final/script.js
@@ -1,4 +1,3 @@
-
 function toggleContent() {
   const extra = document.getElementById("extraContent");
   if (extra.style.display === "block") {
@@ -8,14 +7,25 @@ function toggleContent() {
   }
 }
 
-
 document.addEventListener("DOMContentLoaded", function() {
-  const image = document.querySelector(".images img");
-  image.addEventListener("mouseover", function() {
-    image.style.transition = "transform 0.3s ease";
-    image.style.transform = "scale(1.2)";
+  const images = document.querySelectorAll(".images img");
+  images.forEach(image => {
+    image.addEventListener("mouseover", function() {
+      image.style.transition = "transform 0.3s ease";
+      image.style.transform = "scale(1.2)";
+    });
+    image.addEventListener("mouseout", function() {
+      image.style.transform = "scale(1)";
+    });
   });
-  image.addEventListener("mouseout", function() {
-    image.style.transform = "scale(1)";
+
+  const waterBtn = document.querySelector(".water-btn");
+  waterBtn.addEventListener("click", function(e) {
+    
+    waterBtn.classList.add("ripple");
+
+    setTimeout(() => {
+      waterBtn.classList.remove("ripple");
+    }, 600);
   });
 });
